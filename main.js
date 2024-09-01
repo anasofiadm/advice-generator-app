f0aef53a34a4abf2398804e39c398c5b6a275fc9
anasofiadm <anasofiadominguezmontangie@gmail.com>
2024-09-01 00:36:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-
 document.getElementById('diceButton').addEventListener('click', function() {
     fetch('https://api.adviceslip.com/advice')
         .then(response => response.json())
         .then(data => {
-            alert(data.slip.advice);
+            // Select the element with the class 'advice-text'
+            const adviceTextElement = document.querySelector('.advice-text');
+            
+            // Change the inner text of the selected element to the new advice
+            adviceTextElement.textContent = `"${data.slip.advice}"`;
         })
         .catch(error => {
             console.error('Error fetching the advice:', error);
