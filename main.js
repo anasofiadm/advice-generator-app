dfac58374cde2fc6989eebcb7e576b402fc03d49
anasofiadm <anasofiadominguezmontangie@gmail.com>
2024-09-01 00:21:29 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+ document.getElementById('diceButton').addEventListener('click', function() {
+            fetch('https://api.adviceslip.com/advice')
+                .then(response => response.json())
+                .then(data => {
+                    alert(data.slip.advice);
+                })
+                .catch(error => {
+                    console.error('Error fetching the advice:', error);
+                });
+        });
